31b73ec7fa1ed9b1586fd129f657ba882bb86c42
Leress <jdw751@msstate.edu>
2016-09-16 13:48:39 -0400

    9-16-16 1347
    chnge MACjs, mh, mah

--- app/dev/Maintenance/ctrls/maintenanceAdminCtrl.js
+++ app/dev/Maintenance/ctrls/maintenanceAdminCtrl.js
@@ -1,30 +1,30 @@
 var maintenance = angular.module('maintananceApp', []);
 
-maintenance.controller('maintenanceAdminCtrl', function($scope, ticketFactory){
+maintenance.controller('maintenanceAdminCtrl', function($scope){
 	$scope.ticketSubmission = [];
 	$scope.ticketHistory = [];
 	
 
-	var getAllTickets = function(){
-		ticketFactory.getAllTickets()
-		.then(
-			function(data){
-			$scope.ticketHistory = data.data;
-		},
-			function(){
-				alert('nope');
-		}
-	);
+	// var getAllTickets = function(){
+	// 	ticketFactory.getAllTickets()
+	// 	.then(
+	// 		function(data){
+	// 		$scope.ticketHistory = data.data;
+	// 	},
+	// 		function(){
+	// 			alert('nope');
+	// 	}
+	// );
 
-	};
+	// };
 	//getAllTickets();
 
 	$scope.submitNewTicket  = function(){
 		$scope.ticketSubmission.push({category:$scope.category,description:$scope.description
 			,startDate:'',completeDate:'',status:'Submitted',aptID:'',usr:''});
 
-		console.log(ticketSubmission.category);
-		alert(ticketSubmission.category);
+		console.log($scope.ticketSubmission[0].category);
+		alert($scope.ticketSubmission[0].category);
 	}
 
 
