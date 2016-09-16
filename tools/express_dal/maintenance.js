e44b2a9e3f3810b21b5c17331c2c4caa42fb7a6f
Leress <jdw751@msstate.edu>
2016-09-15 22:40:44 -0400

    ...
    ...

---
+++ tools/express_dal/maintenance.js
@@ -0,0 +1,50 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms';
+
+
+exports.findAllTickets = function(req,res){
+	 MongoClient.connect(url, function(err,db){
+		var collection = db.collection('maintenanceIK');
+		collection.find().toArray(function(err,tickets){
+			if(!err){
+				res.send(tickets);
+			}
+		});
+		db.close();
+	});
+	
+};
+
+exports.submitNewTicket = function(ticket)
+{
+
+ MongoClient.connect(url, function (err, db) 
+ {
+ 	if (err) {
+    	console.log('Unable to connect to the mongoDB server. Error:', err);
+  			} 
+  	else {
+    	var collection =  db.collection('maintenanceIK');
+    	var newTicket = collection.insert
+    	(
+	    	{
+				category: ticket.category,
+				description: ticket.description,
+				startDate: ticket.startDate,
+				completeDate: 0,
+				status: ticket.status,
+				aptID: ticket.aptID,
+				usr: ticket.usr
+
+			}
+
+    	);
+		}
+
+ });
+ 	db.close();
+	
+}
