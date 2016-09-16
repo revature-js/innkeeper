4c130707ecc3831a0a5a5f1cacc4ea7aa81d6450
Leress <jdw751@msstate.edu>
2016-09-16 15:58:47 -0400

    ..
    ..

--- tools/express_dal/server.js
+++ tools/express_dal/server.js
@@ -6,8 +6,6 @@ var app = express();
 
 var reimbursement = require('./reimbursement');
 
-var maintenance = require('./maintenance');
-
 app.use(function(req, res, next) {
   var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
   var origin = req.headers.origin;
@@ -25,15 +23,12 @@ app.use(bodyParser.json());
 
 app.get('/reimbursements', reimbursement.findAllReimbursements);
 app.get('/reimbursements/:id', reimbursement.findReimbursementById);
+app.get('/apartments', apartments.findAllApartments);
+app.get('/apartments/:userName', apartments.findApartmentsByUsername);
 app.post('/reimbursements', reimbursement.addReimbursement);
 app.post('/reimbursements/:id',reimbursement.updateReimbursement);
-
-app.get('/maintenance', maintenance.findAllTickets);
-app.get('/maintenance', maintenance.findTicketByUser);
-app.get('/maintenance', maintenance.getAllCategories);
-app.get('/maintenance', maintenance.getAllApartments);
-app.post('/maintenance', maintenance.submitNewTicket);
-app.post('/maintenance/:id', maintenance.updateTicket)
+app.post('/apartments', apartment.addApartment);
+app.post('/apartments/:userName',apartment.updateApartment)
 
 app.listen(3030);
 console.log('Listening on port 3030...');
