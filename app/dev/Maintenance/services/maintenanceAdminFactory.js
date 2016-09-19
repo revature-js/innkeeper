e473463ab567e2bc15f6471c040e9a73c8f49489
Leress <jdw751@msstate.edu>
2016-09-19 11:06:32 -0400

    9-19-16 1105
    adjust admin factory

--- app/dev/Maintenance/services/maintenanceAdminFactory.js
+++ app/dev/Maintenance/services/maintenanceAdminFactory.js
@@ -1,12 +1,18 @@
-dalDemo.factory('dataFactory', function($http){
-	var getAllTickets = function(){
-		var defer = $q.defer();
-		$http.get('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms').then(function(response){
-			defer.resolve(response.data);
-		}, function(response) {
-			defer.reject(response);
+maintenanceAdmin.factory('dataFactory', function($http){
+	// var getAllTickets = function(){
+	// 	var defer = $q.defer();
+	// 	$http.get('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms').then(function(response){
+	// 		defer.resolve(response.data);
+	// 	}, function(response) {
+	// 		defer.reject(response);
 		
-		});
+	// 	});
 
-		return defer.promise;
-	};
+	// 	return defer.promise;
+	// };
+	var sumbmitTicket = function(){
+		$http.post('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms',
+			)
+		.then(function(response){
+	}
+};
