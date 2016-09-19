265abefd5cbc56106b2c8be8ee28fd710c5439cd
Leress <jdw751@msstate.edu>
2016-09-19 12:26:18 -0400

    9-19-16 1226
    ....

--- app/dev/Maintenance/services/maintenanceAdminFactory.js
+++ app/dev/Maintenance/services/maintenanceAdminFactory.js
@@ -3,6 +3,11 @@ maintenanceAdmin.factory('dataFactory', function($http){
 
 	var fatory = {};
 
+	factory.getAllTickest = function(){
+		$http.get('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms',
+			data);
+	}
+
 	factory.getCategories = function(){
 		return['Request Item','Missing Item','Broken Item'];
 	};
@@ -16,13 +21,15 @@ maintenanceAdmin.factory('dataFactory', function($http){
 
 	};
 
-	factory.updateTicket = function(data){
+	factory.updateTicket = function(data,update){
+		return $http.post('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms',
+			data._id);
 
 	};
 
 
 	factory.sumbmitTicket = function(data){
-		$http.post('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms',
+		return $http.post('mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms',
 			data);
 		
 	}
