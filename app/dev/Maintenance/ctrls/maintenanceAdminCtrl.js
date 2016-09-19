e5361a6c35fc3ffaff3e1c68e7ab8f46c928fdd8
Leress <jdw751@msstate.edu>
2016-09-18 20:35:27 -0400

    9-18-16 2035
    worked on AdminCtrls and Admin Html

--- app/dev/Maintenance/ctrls/maintenanceAdminCtrl.js
+++ app/dev/Maintenance/ctrls/maintenanceAdminCtrl.js
@@ -4,6 +4,7 @@ maintenance.controller('maintenanceAdminCtrl', function($scope){
 	$scope.ticketSubmission = [];
 	$scope.ticketHistory = [];
 	
+	
 
 	// var getAllTickets = function(){
 	// 	ticketFactory.getAllTickets()
@@ -23,7 +24,7 @@ maintenance.controller('maintenanceAdminCtrl', function($scope){
 		$scope.ticketSubmission.push({
 			category:$scope.category,
 			description:$scope.description,
-			startDate:'',
+			startDate:new Date(),
 			completeDate:'',
 			status:'Submitted',
 			aptID:$scope.apartment,
@@ -38,8 +39,8 @@ maintenance.controller('maintenanceAdminCtrl', function($scope){
 			aptID:$scope.apartment,
 			usr:''});
 
-		console.log($scope.ticketSubmission[0].aptID);
-		alert($scope.ticketSubmission[0]);
+		console.log($scope.ticketSubmission.category);
+		alert($scope.ticketSubmission.category);
 	}
 
 
