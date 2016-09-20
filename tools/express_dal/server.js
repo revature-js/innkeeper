fd2bf3c21fb387f68aed28bc11f64e566d0ccae6
Kevin Gutierrez <kgutierrez117@gmail.com>
2016-09-20 14:53:35 -0400

    Completed merge of server.js

--- tools/express_dal/server.js
+++ tools/express_dal/server.js
@@ -6,9 +6,9 @@ var app = express();
 
 var reimbursement = require('./reimbursement');
 
-var maintenance = require('./maintenance');
+//var maintenance = require('./maintenance');
 
-var projections = require('./projections')
+//var projections = require('./projections')
 
 app.use(function(req, res, next) {
   var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000'];
@@ -16,7 +16,6 @@ app.use(function(req, res, next) {
   if(allowedOrigins.indexOf(origin) > -1){
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
@@ -27,10 +26,12 @@ app.use(bodyParser.json());
 
 app.get('/reimbursements', reimbursement.findAllReimbursements);
 app.get('/reimbursements/:id', reimbursement.findReimbursementById);
-//app.get('/apartments', apartments.findAllApartments);
-//app.get('/apartments/:userName', apartments.findApartmentsByUsername);
+app.get('/reimbursement/:username', reimbursement.findReimbursementsByUsername);
 app.post('/reimbursements', reimbursement.addReimbursement);
 app.post('/reimbursements/:id/:decision',reimbursement.updateReimbursement);
+
+//app.get('/apartments', apartments.findAllApartments);
+//app.get('/apartments/:userName', apartments.findApartmentsByUsername);
 //app.post('/apartments', apartment.addApartment);
 //app.post('/apartments/:userName',apartment.updateApartment);
 
@@ -41,8 +42,8 @@ app.post('/reimbursements/:id/:decision',reimbursement.updateReimbursement);
 //app.post('/maintenance', maintenance.submitNewTicket);
 //app.post('/maintenance/:id', maintenance.updateTicket)
 
-app.get('/projections', projections.getAllBatches);
-app.get('/projections', projections.getAllApartments);
+//app.get('/projections', projections.getAllBatches);
+//app.get('/projections', projections.getAllApartments);
 
 app.listen(3030);
 console.log('Listening on port 3030...');
