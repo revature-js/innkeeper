0f3490a9a12b6536f0ff8db29b9b7f80cfb489d5
Leress <jdw751@msstate.edu>
2016-09-20 14:16:14 -0400

    9-20-16 1414
    add getTikBU, getAll, submit, update

--- tools/express_dal/maintenance.js
+++ tools/express_dal/maintenance.js
@@ -29,44 +29,43 @@ exports.getTicketsByUser = function(req, res){
 	});
 };
 
-exports.submitNewTicket = function($scope)
+exports.submitNewTicket = function(req,res)
 {
+	var object = req.body;
 
- MongoClient.connect(url, function (err, db) 
- {
- 	if (err) {
-    	console.log('Unable to connect to the mongoDB server. Error:', err);
-  			} 
-  	else {
-    	var collection =  db.collection('maintenanceIK');
-    	var newTicket = collection.insert
-    	(
-	    	{
-				category: $scope.category,
-				description: $scope.description,
-				startDate: $scope.startDate,
-				completeDate: $scope.completeDate,
-				status: $scope.status,
-				aptID: $scope.aptID,
-				usr: $scope.usr
+	if (object.usr == undefined || object.aptID == undefined)
+        throw "addTicket: Usr and aptID cannot be undefined";
 
+    if (object.status == undefined)
+        object.status = "Submitted";
+    if (object.startDate == undefined)
+        object.startDate = new Date();
+
+
+	 MongoClient.connect(url, function (err, db) 
+	 {
+	 	if (err) {
+	    	console.log('Unable to connect to the mongoDB server. Error:', err);
+	  			} 
+	  	else {
+	    	var collection =  db.collection('maintenanceIK');
+	    	collection.save(object);
 			}
 
-    	);
-		}
-
- });
+ 	});
  	db.close();
 	
 };
 
 
 exports.updateTicket = function(req,res){
-	MongoClient.connect(url, function (err, db){
-		var id = req.param.id;
-		var update = req.param.status;
-		var collection = db.collection('maintenanceIK');
-		collection.updateOne({'_id':ObjectID(id),{$set:{'status':update}}, function(err, result){
+		var id = req.body.id;
+		var update = req.body;
+
+		MongoClient.connect(url, function (err, db){
+		
+			var collection = db.collection('maintenanceIK');
+			collection.updateOne({'_id':ObjectID(id),{$set:{'status':update}}, function(err, result){
 			if(err){
 				res.send('error');
 			}
@@ -79,3 +78,15 @@ exports.updateTicket = function(req,res){
 	});
 	};
 };
+
+exports.getTicketById = function(req,res){
+	
+	MongoClient.connect(url, function(err,db) {
+        var objectId = req.query.ticket_id;
+        var c = db.collection('maintenanceIK');
+        c.findOne ( {"_id": new ObjectId(objectId)}, function(err,ticket){
+            res.send(ticket);
+        });
+        db.close();
+    });
+}
