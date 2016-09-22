57bdfd26eeb59bef5ff65494e19ce12d422f91bb
WestonRWright <wrightw@uindy.edu>
2016-09-22 17:09:39 -0400

    D

--- tools/express_dal/server.js
+++ tools/express_dal/server.js
@@ -7,21 +7,21 @@ var LocalStrategy = require('passport-local').Strategy;
 var expressValidator = require('express-validator');
 var bcrypt = require('bcryptjs');
 //------------------------------ TOKENS ------------------------------
-const fs = require('fs');
-var jwt = require('jsonwebtoken');
-var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+// const fs = require('fs');
+// var jwt = require('jsonwebtoken');
+// var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 
-//backdate a jwt 30 seconds
-var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30 }, 'shhhhh');
+// //backdate a jwt 30 seconds
+// var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30 }, 'shhhhh');
 
-// sign with RSA SHA256
-//var cert = fs.readFileSync('private.key');  // get private key
-// var token = jwt.sign({ foo: 'bar' }, cert, { algorithm: 'RS256'});
-cert = "secret";
-// sign asynchronously
-jwt.sign({ foo: 'bar' }, cert, { algorithm: 'RS256' }, function(err, token) {
-  console.log(token);
-});
+// // sign with RSA SHA256
+// //var cert = fs.readFileSync('private.key');  // get private key
+// // var token = jwt.sign({ foo: 'bar' }, cert, { algorithm: 'RS256'});
+// cert = "secret";
+// // sign asynchronously
+// jwt.sign({ foo: 'bar' }, cert, { algorithm: 'RS256' }, function(err, token) {
+//   console.log(token);
+// });
 //---------------------------- END TOKENS ----------------------------
 
 var reimbursement = require('./reimbursement');
