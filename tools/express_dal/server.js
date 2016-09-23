fcdb3b6f7f97c6e6315b0696faf58128e937f7e5
Kevin Gutierrez <kgutierrez117@gmail.com>
2016-09-23 14:20:11 -0400

    Cleaned up the dev

--- tools/express_dal/server.js
+++ tools/express_dal/server.js
@@ -1,77 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var router = express.Router();
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
-var LocalStrategy = require('passport-local').Strategy;
-var expressValidator = require('express-validator');
-var bcrypt = require('bcryptjs');
 
 var reimbursement = require('./reimbursement');
 var maintenance = require('./maintenance');
 var reimbursement = require('./reimbursement');
 var loginRegister = require('./loginRegister.js');
-
 var apartments = require('./apartments');
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-  userObj.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
-
-passport.use(new LocalStrategy(
-  function(username,password,done) {
-    client.connect(url, function(err,db){
-      var collection = db.collection('usersIK');
-      collection.findOne({username:username}, function(err, user){
-        if (err){return done(err);}
-        if (!user){
-          return done(null, false, {message: 'Incorrect username.'});
-        }
-        return done(null, user);
-      });
-    });
-  }
-));
 
 var app = express();
 
-var mongo = require('mongodb');
-var client = mongo.MongoClient;
-var url = 'mongodb://innkeeper:inn123@ds017636.mlab.com:17636/rlms';
-var ObjectID = mongo.ObjectID;
-
-var userObj = {
-  username: {
-            type: String,
-            index: true
-  },
-  password: {type: String},
-  email: {type: String},
-  fname: {type: String},
-  lname: {type: String},
-  isAdmin: {type: String},
-  aptId: {type: String},
-  batch: {type: String}
-};
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-
-app.use(require('express-session')({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-}));
-
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(function(req, res, next) {
   var allowedOrigins = ['http://127.0.0.1:3000', 'http://localhost:3000', 'http://ec2-54-218-76-216.us-west-2.compute.amazonaws.com', 'http://ec2-54-218-76-216.us-west-2.compute.amazonaws.com:3030', 'http://ec2-54-218-76-216.us-west-2.compute.amazonaws.com:3000'];
@@ -108,33 +46,5 @@ app.post('/createUser' , loginRegister.createUser);
 app.post('/login/:username/:aptId',apartments.updateAptID);
 app.get('/login', loginRegister.allUsernames);
 
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
-};
-
-app.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { 
-      return res.redirect('/login'); 
-    }
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
-      res.status(200).json({
-       user
-     });
-    });
-  })(req, res, next);
-});
-
-module.exports = app;
-
 app.listen(3030);
-
 console.log('Listening on port 3030...');
